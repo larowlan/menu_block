98c0b38cf6cef3a216294ac8b5d5789f595fa238
JohnAlbin <virtually.johnalbin@gmail.com>
2008-11-24 05:20:11 +0000

    #333988: Create template for menu-block-wrapper

---
+++ menu-block-wrapper.tpl.php
@@ -0,0 +1,24 @@
+<?php
+// $Id$
+
+/**
+ * @file menu-block-wrapper.tpl.php
+ * Default theme implementation to wrap menu blocks.
+ *
+ * Available variables:
+ * - $content: The unordered list containing the menu.
+ * - $classes: A string containing the CSS classes for the DIV tag. Includes:
+ *   menu-name-NAME, menu-level-LEVEL, menu-depth-DEPTH, and menu-expanded.
+ * - $classes_array: An array containing each of the CSS classes.
+ *
+ * The following variables are provided for contextual information.
+ * - $settings: An array of the block's settings. Includes menu_name, level,
+ *   depth, and expanded.
+ *
+ * @see template_preprocess_menu_block_wrapper()
+ * @see theme_menu_block_wrapper()
+ */
+?>
+<div class="<?php print $classes; ?>">
+  <?php print $content; ?>
+</div>
