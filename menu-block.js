209a10e3688a75f373623ff4af66bc85954b9cf9
JohnAlbin <virtually.johnalbin@gmail.com>
2009-04-09 06:00:05 +0000

    #345419: Add option for menu title as link.

--- menu-block.js
+++ menu-block.js
@@ -1,6 +1,19 @@
 // $Id$
 
 $(document).ready( function() {
+  // Toggle display of "title link" if "block title" has a value.
+  $('#edit-title').change( function() {
+    if ($('#edit-title').val()) {
+      $('#edit-title-link-wrapper').slideUp('fast');
+    }
+    else {
+      $('#edit-title-link-wrapper').slideDown('fast');
+    }
+  } );
+  if ($('#edit-title').val()) {
+    $('#edit-title-link-wrapper').css('display', 'none');
+  }
+  // Split the un-wieldly "parent item" pull-down into two hierarchal pull-downs.
   $('#edit-parent')
     .html(Drupal.settings.menu_block.parent_options[Drupal.settings.menu_block.menus_default])
     .val(Drupal.settings.menu_block.parent_default)
@@ -10,12 +23,13 @@ $(document).ready( function() {
       .html(Drupal.settings.menu_block.parent_options[$('#edit-parent-menu').val()])
       .val(Drupal.settings.menu_block.parent_default);
   } );
+  // Toggle display of "follow parent" if "follow" has been checked.
   $('#edit-follow').change( function() {
     if ($('#edit-follow:checked').length) {
-      $('#edit-follow-parent-wrapper').slideDown('slow');
+      $('#edit-follow-parent-wrapper').slideDown('fast');
     }
     else {
-      $('#edit-follow-parent-wrapper').slideUp('slow');
+      $('#edit-follow-parent-wrapper').slideUp('fast');
     }
   } );
   if (!$('#edit-follow:checked').length) {
