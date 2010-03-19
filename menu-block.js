6ae94e91946baf08ec7906728ba7f4dc7e3a9630
JohnAlbin <virtually.johnalbin@gmail.com>
2010-03-19 07:47:38 +0000

    #741284 by JohnAlbin, sdboyer, and hefox: Add "menu tree" content types to Chaos Tools/Panels.

--- menu-block.js
+++ menu-block.js
@@ -9,29 +9,47 @@ Drupal.behaviors.menu_block = function (context) {
   }
   $('#menu-block-settings', context).addClass('menu-block-processed');
 
-  // Toggle display of "title link" if "block title" has a value.
-  $('#edit-title', context).change( function() {
-    if ($('#edit-title').val()) {
-      $('#edit-title-link-wrapper').slideUp('fast');
+  // Process the form if its in a Panel overlay.
+  if ($('#override-title-checkbox', context).size()) {
+    // Toggle display of "title link" if "override title" is checked.
+    $('#override-title-checkbox', context).change( function() {
+      if ($('#override-title-checkbox:checked').length) {
+        $('#edit-title-link-wrapper').slideUp('fast');
+      }
+      else {
+        $('#edit-title-link-wrapper').slideDown('fast');
+      }
+    } );
+    if ($('#override-title-checkbox:checked').length) {
+      $('#edit-title-link-wrapper').css('display', 'none');
     }
-    else {
-      $('#edit-title-link-wrapper').slideDown('fast');
-    }
-  } );
-  if ($('#edit-title', context).val()) {
-    $('#edit-title-link-wrapper').css('display', 'none');
   }
+  // Process the form if its on a block config page.
+  else {
+    // Toggle display of "title link" if "block title" has a value.
+    $('#edit-title', context).change( function() {
+      if ($('#edit-title').val()) {
+        $('#edit-title-link-wrapper').slideUp('fast');
+      }
+      else {
+        $('#edit-title-link-wrapper').slideDown('fast');
+      }
+    } );
+    if ($('#edit-title', context).val()) {
+      $('#edit-title-link-wrapper').css('display', 'none');
+    }
 
-  // Split the un-wieldly "parent item" pull-down into two hierarchal pull-downs.
-  $('#edit-parent', context)
-    .html(Drupal.settings.menu_block.parent_options[Drupal.settings.menu_block.menus_default])
-    .val(Drupal.settings.menu_block.parent_default)
-    .before(Drupal.settings.menu_block.menus);
-  $('#edit-parent-menu', context).change( function() {
-    $('#edit-parent')
-      .html(Drupal.settings.menu_block.parent_options[$('#edit-parent-menu').val()])
-      .val(Drupal.settings.menu_block.parent_default);
-  } );
+    // Split the un-wieldly "parent item" pull-down into two hierarchal pull-downs.
+    $('#edit-parent', context)
+      .html(Drupal.settings.menu_block.parent_options[Drupal.settings.menu_block.menus_default])
+      .val(Drupal.settings.menu_block.parent_default)
+      .before(Drupal.settings.menu_block.menus);
+    $('#edit-parent-menu', context).change( function() {
+      $('#edit-parent')
+        .html(Drupal.settings.menu_block.parent_options[$('#edit-parent-menu').val()])
+        .val(Drupal.settings.menu_block.parent_default);
+    } );
+  }
 
   // Toggle display of "follow parent" if "follow" has been checked.
   $('#edit-follow', context).change( function() {
