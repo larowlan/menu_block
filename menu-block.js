35c5af4f3246d166f20d8b683d490478196aff52
JohnAlbin <virtually.johnalbin@gmail.com>
2010-08-06 07:03:58 +0000

    Added jQuery UI buttonset styling to display options form element.

--- menu-block.js
+++ menu-block.js
@@ -21,6 +21,12 @@ Drupal.behaviors.menu_block = {
         .val(Drupal.settings.menu_block.parent_default);
     } );
 
+    // Show the "display options" if javascript is on.
+    $('.form-item-display-options.form-type-radios>label', context).addClass('element-invisible');
+    $('.form-item-display-options.form-type-radios', context).show();
+    // Make the radio set into a jQuery UI buttonset.
+    $('#edit-display-options', context).buttonset();
+
     // Override the default show/hide animation for Form API states.
     $('#menu-block-settings', context).bind('state:visible', function(e) {
       if (e.trigger) {
