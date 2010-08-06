5e012bf752993a9f8b4f1004f39d4f41f6836be3
JohnAlbin <virtually.johnalbin@gmail.com>
2010-08-06 07:00:06 +0000

    Removed custom JS checking of panels/block form differences.

--- menu-block.js
+++ menu-block.js
@@ -10,19 +10,16 @@ Drupal.behaviors.menu_block = {
     }
     $('#menu-block-settings', context).addClass('menu-block-processed');
 
-    // Process the form if its on a block config page (not in a Panel overlay.)
-    if ($('.menu-block-configure-form', context).size()) {
-      // Split the un-wieldly "parent item" pull-down into two hierarchal pull-downs.
-      $('.menu-block-parent', context)
-        .html(Drupal.settings.menu_block.parent_options[Drupal.settings.menu_block.menus_default])
-        .val(Drupal.settings.menu_block.parent_default)
-        .before(Drupal.settings.menu_block.menus);
-      $('.menu-block-parent-menu', context).change( function() {
-        $('.menu-block-parent')
-          .html(Drupal.settings.menu_block.parent_options[$('.menu-block-parent-menu').val()])
-          .val(Drupal.settings.menu_block.parent_default);
-      } );
-    }
+    // Split the un-wieldly "parent item" pull-down into two hierarchal pull-downs.
+    $('.menu-block-parent', context)
+      .html(Drupal.settings.menu_block.parent_options[Drupal.settings.menu_block.menus_default])
+      .val(Drupal.settings.menu_block.parent_default)
+      .before(Drupal.settings.menu_block.menus);
+    $('.menu-block-parent-menu', context).change( function() {
+      $('.menu-block-parent')
+        .html(Drupal.settings.menu_block.parent_options[$('.menu-block-parent-menu').val()])
+        .val(Drupal.settings.menu_block.parent_default);
+    } );
 
     // Toggle display of "follow parent" if "follow" has been checked.
     $('.menu-block-follow', context).change( function() {
