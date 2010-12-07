6527a1bddf032f46aed90485840cfd8a175c2dfe
JohnAlbin <virtually.johnalbin@gmail.com>
2010-12-07 14:05:15 +0000

    #825132: Add hook_menu_block_get_sort_menus() for improved performance and UX of book integration

--- menu_block.api.php
+++ menu_block.api.php
@@ -72,5 +72,26 @@ function hook_get_menus() {
 }
 
 /**
+ * Return a list of menus to use on menu block's settings form.
+ *
+ * Menu block's settings page sorts menus for use with its "the menu selected by
+ * the page" option.
+ *
+ * @return
+ *   An array containing the menus' machine names as keys with their menu titles
+ *   as values. The key may optionally be a regular expression to match several
+ *   menus at a time; see book_menu_block_get_sort_menus() for an example.
+ */
+function hook_menu_block_get_sort_menus() {
+  $menus = array();
+  // For each menu, add the following information:
+  $menus['menu_name'] = 'menu title';
+  // Optionally, add a regular expression to match several menus at once.
+  $menus['/^my\-menus\-.+/'] = t('My menus');
+
+  return $menus;
+}
+
+/**
  * @} End of "addtogroup hooks".
  */
